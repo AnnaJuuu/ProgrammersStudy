d93f5cca669da03011ba53ad80d84c52b56edd4a
Anna <167738708+AnnaJuuu@users.noreply.github.com>
2024-06-03 13:46:49 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 61 MB -BaekjoonHub

--- Kotlin/프로그래머스/0/120829. 각도기/각도기.kt
+++ Kotlin/프로그래머스/0/120829. 각도기/각도기.kt
@@ -1,14 +1,12 @@
-class Solution 
-{
-    fun solution(angle: Int): Int 
-    {
-        when (angle) 
-        {
-            in 1 .. 89 -> return 1
-            90 -> return 2
-            in 91 .. 179 -> return 3
-            180 -> return 4
+class Solution {
+    fun solution(angle: Int): Int {
+        return when(angle) {
+            in 1..89 -> 1
+            90 -> 2
+            in 91..179 -> 3
+            180 -> 4
+            else -> 0  
         }
-        return 0
     }
 }
+
