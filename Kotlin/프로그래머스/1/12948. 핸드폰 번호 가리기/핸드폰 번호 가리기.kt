2d4604fbc79b44e6f8081c580698cb0ab706ec04
Anna <167738708+AnnaJuuu@users.noreply.github.com>
2024-07-01 09:55:09 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 10.76 ms, Memory: 60.5 MB -BaekjoonHub

--- Kotlin/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
+++ Kotlin/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
@@ -1,14 +1,5 @@
-class Solution 
-{
-    fun solution(phone_number: String): String 
-    {
-        var answer = ""
-        var length = phone_number.length -4
-        for(i in 1..length)
-        {
-            answer += "*"
-        }
-        answer = answer + phone_number.substring(length, length + 4)
-        return answer
+class Solution {
+    fun solution(phone_number: String): String {
+        return "${"".padStart(phone_number.length - 4, '*')}${phone_number.takeLast(4)}"
     }
 }
