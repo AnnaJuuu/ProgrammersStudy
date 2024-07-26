37c9d7f6fc36667860277cbd4129659465b06fd3
Anna <167738708+AnnaJuuu@users.noreply.github.com>
2024-07-26 10:58:35 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 28.58 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.kt
@@ -0,0 +1,16 @@
+class Solution
+{
+    fun solution(n: Int): String
+    {
+        var answer = ""
+        if(n % 2 == 0)
+        {
+            for(i in 0 until (n/2)) answer += "수박"
+        } else
+        {
+            for(i in 0 until (n/2)) answer += "수박"
+            answer += "수"
+        }
+        return answer
+    }
+}
